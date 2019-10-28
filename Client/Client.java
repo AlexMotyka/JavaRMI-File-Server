1af742d72661ba2e2025bda31b70e5275163b666
AlexMotyka <motykaalex@gmail.com>
2019-10-28 14:03:57 -0400

    Fix class name error

--- Client/Client.java
+++ Client/Client.java
@@ -1,11 +1,13 @@
 import java.io.*; 
 import java.rmi.*;
 
-public class FileClient{
+public class Client{
    public static void main(String argv[]) {
       try {
 		  String name = "//" + argv[1] + "/Server";
 		  ServerInterface si = (ServerInterface) Naming.lookup(name);
+		  String response = si.echo("hello");
+		  System.out.println(response);
       } catch(Exception e) {
          System.err.println("Server exception: "+ e.getMessage());
          e.printStackTrace();
