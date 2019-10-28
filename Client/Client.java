e98128b1aad35cef1754d41019caa471c752762b
AlexMotyka <motykaalex@gmail.com>
2019-10-28 14:17:29 -0400

    Add while loop for client

--- Client/Client.java
+++ Client/Client.java
@@ -1,12 +1,18 @@
 import java.io.*; 
 import java.rmi.*;
+import java.util.Scanner;
 
 public class Client{
    public static void main(String argv[]) {
       try {
 		  ServerInterface si = (ServerInterface) Naming.lookup("Server");
-		  String response = si.echo("hello");
-		  System.out.println(response);
+		  Scanner userIn = new Scanner(System.in);
+		  while(true){
+			  System.out.println("Enter a string: ");
+			  String input = userIn.nextLine();
+			  String response = si.echo(input);
+			  System.out.println(response);
+		  }
       } catch(Exception e) {
          System.err.println("Server exception: "+ e.getMessage());
          e.printStackTrace();
