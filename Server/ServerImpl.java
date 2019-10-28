7f7aa75234761f785959333ede6dd863f2b99607
AlexMotyka <motykaalex@gmail.com>
2019-10-28 15:42:36 -0400

    Add download

--- Server/ServerImpl.java
+++ Server/ServerImpl.java
@@ -14,4 +14,19 @@ public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
    public String echo(String input) {
 	   return input;
    }
+   public byte[] downloadFile(String filename){
+	  
+      try {
+         File file = new File(filename);
+         byte buffer[] = new byte[(int)file.length()];
+         BufferedInputStream input = new BufferedInputStream(new FileInputStream(filename));
+         input.read(buffer,0,buffer.length);
+         input.close();
+         return(buffer);
+      } catch(Exception e){
+         System.out.println("ServerImpl: "+e.getMessage());
+         e.printStackTrace();
+         return(null);
+      }
+   }
 }
