1c8af4c2416aabfa9f153dfb3aa346889f54dd6b
AlexMotyka <motykaalex@gmail.com>
2019-10-28 13:47:00 -0400

    Upddate interface variable name

--- Server/Server.java
+++ Server/Server.java
@@ -4,8 +4,8 @@ import java.rmi.*;
 public class Server {
    public static void main(String argv[]) {
       try {
-         ServerInterface inter = new ServerImpl("Server");
-         Naming.rebind("//127.0.0.1/Server", inter);
+         ServerInterface si = new ServerImpl("Server");
+         Naming.rebind("//127.0.0.1/Server", si);
 		 System.err.println("Server is ready!");
       } catch(Exception e) {
          System.out.println("Server: "+e.getMessage());
